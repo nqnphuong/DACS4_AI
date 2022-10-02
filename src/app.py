06bf04a86c69429bf0df9ca62f37f9361d8c6e54
nqnphuong <ngnhatphuong.a.h@gmail.com>
2022-10-02 20:55:40 +0700

    chuyển giọng nói thành văn bản

---
+++ src/app.py
@@ -0,0 +1,4 @@
+from src.soundProcessing.AudioToWord import get_audio
+
+if __name__ == '__main__':
+    get_audio()
---
+++ src/soundProcessing/AudioToWord.py
@@ -0,0 +1,15 @@
+import speech_recognition as sr
+
+
+def get_audio():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Tôi: ", end='')
+        audio = r.listen(source, phrase_time_limit=5)
+        try:
+            text = r.recognize_google(audio, language="vi-VN")
+            print(text)
+            return text
+        except:
+            print("...")
+            return 0
